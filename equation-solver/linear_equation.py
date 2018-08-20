75d6becb5133ac2759c9b0e845ff45961a4533e4
GabriellBP <gbp@ic.ufal.br>
2018-08-19 21:55:08 -0300

    Solving bugs on parsing function when there are no variables in one side on equation or when the results of operations with variable are zero

--- equation-solver/linear_equation.py
+++ equation-solver/linear_equation.py
@@ -21,7 +21,7 @@ def separate_variables(string):
 
 def verify_op(regex, string, pos):
     for i in regex:
-        print(i, string[pos])
+        # print(i, string[pos])
         if not re.match(i, string[pos]):
             return False
         pos += 1
@@ -30,9 +30,9 @@ def verify_op(regex, string, pos):
 
 # transforms 10*x+20*x+30*x to 10+20+30
 def remove_var(string):
-    print(string)
+    # print(string)
     string = string.replace("*x", '')
-    print(string)
+    # print(string)
     return string
 
 
@@ -42,14 +42,19 @@ def resolve(string):
 
 
 def parsing(eq):
+    if "x" not in eq:
+        return 0, eval(eq)
     eq = separate_variables(eq)
     regex_var = r"((?:\+|\-)?\d*(?:\*|\/)[a-zA-Z])"
     var_op = re.findall(regex_var, eq)
+    print(var_op)
     var_op = ''.join(var_op)
     var_op = remove_var(var_op)
     var_value = eval(var_op)
     regex_const = r"((?:\+|\-|\*|\/)?\d+)(?!(\d*)(\*|\/)[a-zA-Z])"
     const_op = re.findall(regex_const, eq)
+    if len(const_op) == 0:
+        return var_value, 0
     const_op = [i[0] for i in const_op]  # return a list of te first`s elements of list of tuple
     const_op = ''.join(const_op)
     const_value = eval(const_op)
@@ -63,14 +68,19 @@ def solve_linear_equation(eq):
     after_equals_eq = eq[1]
     before_equals_var_value, before_equals_const_value = parsing(before_equals_eq)
     after_equals_var_value, after_equals_const_value = parsing(after_equals_eq)
-    parsing(before_equals_eq)
-    result = (after_equals_const_value - before_equals_const_value) / (before_equals_var_value - after_equals_var_value)
-    print("x =", result)
+    numerator = after_equals_const_value - before_equals_const_value
+    denominator = before_equals_var_value - after_equals_var_value
+    if denominator == 0:
+        print("Erro: Coeficiente da variável é zero")
+    else:
+        result = numerator / denominator
+        print("x =", result)
 
 
 def main():
-    equation = "2x + 5 - 3x + 4 * 5 = 3 * 2 - 3x + 4"
-    solve_linear_equation(equation)
+    equations = ["0 = 1 + 1x + 1x + 2", "1x = -1", "(1x+5)=-5", "((1+2)/2)*2+1x=1707"]
+    for eq in equations:
+        solve_linear_equation(eq)
 
 
 if __name__ == '__main__':
