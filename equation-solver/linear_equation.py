fa742fa6a63b075a227d41fedb199b111a64ca80
GabriellBP <gbp@ic.ufal.br>
2018-08-20 10:30:24 -0300

    resolving bug in input with '*' between coefficient  and variable adding solution for fractional  coefficient

--- equation-solver/linear_equation.py
+++ equation-solver/linear_equation.py
@@ -11,7 +11,7 @@ def separate_variables(string):
     string = [i for i in re.split(r'([\d.]+|\W+)', string) if i]
     i = 0
     while i < len(string) - 1:
-        if re.match(r'([a-zA-Z])', string[i + 1]):
+        if re.match(r'([a-zA-Z])', string[i + 1]) and not re.match(r'(\W+)', string[i]):
             string = string[:i + 1] + list("*") + string[i + 1:]
             i += 1
         i += 1
@@ -36,6 +36,11 @@ def remove_var(string):
     return string
 
 
+def remove_part(string, part):
+    string = string.replace(part, '')
+    return string
+
+
 def resolve(string):
     # TODO
     print(string)
@@ -45,18 +50,18 @@ def parsing(eq):
     if "x" not in eq:
         return 0, eval(eq)
     eq = separate_variables(eq)
-    regex_var = r"((?:\+|\-)?\d*(?:\*|\/)[a-zA-Z])"
+    regex_var = r"((?:\+|\-)?\d*(?:\*|\/)[a-zA-Z](?:(?:\/|\*)?\d+)*)"
     var_op = re.findall(regex_var, eq)
-    print(var_op)
     var_op = ''.join(var_op)
+    const_op = remove_part(eq, var_op)
     var_op = remove_var(var_op)
     var_value = eval(var_op)
-    regex_const = r"((?:\+|\-|\*|\/)?\d+)(?!(\d*)(\*|\/)[a-zA-Z])"
-    const_op = re.findall(regex_const, eq)
+    # regex_const = r"((?:\+|\-|\*|\/)?\d+)(?!(\d*)(\*|\/)[a-zA-Z])"
+    # const_op = re.findall(regex_const, eq)
     if len(const_op) == 0:
         return var_value, 0
-    const_op = [i[0] for i in const_op]  # return a list of te first`s elements of list of tuple
-    const_op = ''.join(const_op)
+    # const_op = [i[0] for i in const_op]  # return a list of te first`s elements of list of tuple
+    # const_op = ''.join(const_op)
     const_value = eval(const_op)
     return var_value, const_value
 
@@ -78,9 +83,11 @@ def solve_linear_equation(eq):
 
 
 def main():
-    equations = ["0 = 1 + 1x + 1x + 2", "1x = -1", "(1x+5)=-5", "((1+2)/2)*2+1x=1707"]
+    equations = ["0 = 1 + 1x + 1x + 2", "1x = -1", "(1x+5)=-5", "((1+2)/2)*2+1x=1707", "((1+2)/2)*2+5*x/50=1707"]
     for eq in equations:
         solve_linear_equation(eq)
+    # equation = input()
+    # solve_linear_equation(equations)
 
 
 if __name__ == '__main__':
